68c185609afcd849eed302bf09ed491d6dc7d438
cofa <cofa@MacBook-Pro.local>
2020-10-21 21:47:58 +0300

    Click on refresh button when its needed

--- whatsapp.py
+++ whatsapp.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 
 
 class WhatsApp:
     driver = webdriver.Firefox()
+    driver.get("https://web.whatsapp.com")
 
     def wait_for_auth(self):
         qr = self.get_qr_element()
-        qr_code_id = qr.get_attribute('data-ref')
-        qr_count = 1
+        qr_code_id = ''
+        qr_count = 0
         while True:
             current_qr_code_id = qr.get_attribute('data-ref')
-            print(current_qr_code_id)
+            if self.need_to_refresh():
+                qr.click()
+                qr = self.get_qr_element()
             if current_qr_code_id != qr_code_id:
                 qr_code_id = current_qr_code_id
                 qr.screenshot('qr.png')
@@ -20,8 +24,14 @@ class WhatsApp:
                 print(qr_count)
             sleep(2)
 
+    def need_to_refresh(self):
+        try:
+            self.driver.find_element_by_xpath("//span[@data-testid='refresh-large']")
+            return True
+        except NoSuchElementException:
+            return False
+
     def get_qr_element(self):
-        self.driver.get("https://web.whatsapp.com")
         qr = WebDriverWait(self.driver, 10).until(
                 lambda x: x.find_element_by_css_selector("div[data-ref]")
                 )
