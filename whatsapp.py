3f45932ca2ce1a28d8d79bfa32c630008fc79e4b
cofa <cofa@MacBook-Pro.local>
2020-10-22 21:25:50 +0300

    Add autentithicaton condition checking

--- whatsapp.py
+++ whatsapp.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
 from time import sleep
 
 
@@ -9,20 +9,30 @@ class WhatsApp:
     driver.get("https://web.whatsapp.com")
 
     def wait_for_auth(self):
-        qr = self.get_qr_element()
-        qr_code_id = ''
-        qr_count = 0
-        while True:
-            current_qr_code_id = qr.get_attribute('data-ref')
-            if self.need_to_refresh():
-                qr.click()
-                qr = self.get_qr_element()
-            if current_qr_code_id != qr_code_id:
-                qr_code_id = current_qr_code_id
-                qr.screenshot('qr.png')
-                qr_count += 1
-                print(qr_count)
-            sleep(2)
+        try:
+            qr = self.get_qr_element()
+            qr_code_id = ''
+            qr_count = 0
+            while True:
+                current_qr_code_id = qr.get_attribute('data-ref')
+                if self.need_to_refresh():
+                    qr.click()
+                    qr = self.get_qr_element()
+                if current_qr_code_id != qr_code_id:
+                    qr_code_id = current_qr_code_id
+                    qr.screenshot('qr.png')
+                    qr_count += 1
+                sleep(2)
+        except (StaleElementReferenceException, TimeoutException):
+            if self.is_authentithicated():
+                return True
+            self.wait_for_auth()
+
+    def get_qr_element(self):
+        qr = WebDriverWait(self.driver, 10).until(
+                lambda x: x.find_element_by_css_selector("div[data-ref]")
+                )
+        return qr
 
     def need_to_refresh(self):
         try:
@@ -31,13 +41,15 @@ class WhatsApp:
         except NoSuchElementException:
             return False
 
-    def get_qr_element(self):
-        qr = WebDriverWait(self.driver, 10).until(
-                lambda x: x.find_element_by_css_selector("div[data-ref]")
-                )
-        return qr
+    def is_authentithicated(self):
+        try:
+            x = self.driver.find_element_by_xpath("//span[@data-icon='chat']")
+            return True
+        except NoSuchElementException:
+            return False
 
 
 if __name__ == '__main__':
     wa = WhatsApp()
     wa.wait_for_auth()
+    wa.driver.close()
